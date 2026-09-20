7770ddeb61271e87df2c8f0ecffa4378992bfb27
Parkjunbeom99 <pjb990@gmail.com>
2026-09-21 05:38:29 +0900

    Ch1-1.  간단한 이미지 출력 코드

---
+++ CUDAPortfolio/main.cu
@@ -0,0 +1,32 @@
+#include<iostream>
+
+int main()
+{
+	//이미지
+	int ImageWidth = 256;
+	int ImageHeight = 256;
+
+	//렌더
+	std::cout << "P3\n" << ImageWidth << ' ' << ImageHeight << "\n255\n";
+
+
+	for (int j = 0; j < ImageHeight; j++)
+	{
+		std::clog << "\rScanlines remaining " << (ImageHeight - j) << ' ' << std::flush;
+		for (int i = 0; i < ImageWidth; i++)
+		{
+			auto r = double(i) / (ImageWidth - 1);
+			auto g = double(j) / (ImageHeight - 1);
+			auto b = 0.0;
+
+			int ir = int(255.999 * r);
+			int ig = int(255.999 * g);
+			int ib = int(255.999 * b);
+
+			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+
+		}
+	}
+	
+	return 0;
+}
